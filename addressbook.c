19d29f7e6f904ee5826b61fb6559dbefe679d67e
shun52hal <45968749+shun52hal@users.noreply.github.com>
2019-02-13 17:07:30 +0900

    Update addressbook.c

--- addressbook.c
+++ addressbook.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define PARSONS 2
+
 
 typedef struct{
-    char name[200];
-    char gender[100];
+    char name[256];
+    int gender;
     int age;
     int birthday;
     char address[1000];
@@ -16,25 +16,41 @@ typedef struct{
 void address_book_inputs(address_book *data);/*入力関数のプロトタイプ宣言*/
 void address_book_outputs(address_book data);/*出力関数のプロトタイプ宣言*/
 
-int main(void)
-{
-    address_book data[2];
-    int i;
+int main(void){
+    
+    int i,count,datasize;
     
     FILE *file;
     file=fopen("addressbook.csv", "w");
-    fprintf(file,"名前,性別,年齢,生年月日,住所\n");
+    fprintf(file,"名前,性別(1:男性　2:女性),年齢,生年月日,住所\n");
     
+    address_book *data;
     
-    for (i=0; i<PARSONS; i++) {
-        address_book_inputs(&data[i]);
+    datasize=10;
+    data = (address_book*)malloc(sizeof(address_book) * datasize);
+    
+    count= 0;
+    while(1){
+        address_book_inputs(&data[count]);
+        if (data[count].birthday==0)break; /*誕生日に０を入力すると終了*/
+        
+        count++;
+        if (count>=datasize) {
+            datasize += 10;
+            data = (address_book*)reallocf(data, sizeof(address_book)*datasize);
+        }
     }
     
-    for (i=0; i<PARSONS; i++) {
+    for(i=0;i<count;i++){
         address_book_outputs(data[i]);
-        fprintf(file, "%s,%s,%d,%d,%s\n",data[i].name,data[i].gender,data[i].age,data[i].birthday,data[i].address);
+        fprintf(file, "%s,%d,%d,%d,%s\n",data[i].name,data[i].gender,data[i].age,data[i].birthday,data[i].address);
+        fclose(file);
     }
-    fclose(file);
+    
+    free(data);
+    
+    return 0;
+    
 }
 
 /*データを入力する*/
@@ -43,7 +59,7 @@ void address_book_inputs(address_book *data){
     scanf("%s",data->name);/*名前を入力*/
     
     printf("性別(1:男性　2:女性)：");
-    scanf("%s",data->gender);
+    scanf("%d",&data->gender);
     
     printf("年齢：");
     scanf("%d",&data->age);
@@ -58,6 +74,9 @@ void address_book_inputs(address_book *data){
 /*入力したデータを出力する*/
 void address_book_outputs(address_book data){
     
-    printf("名前,性別,年齢,生年月日,住所\n");
-    printf("%s,%s,%d,%d,%s\n",data.name,data.gender,data.age,data.birthday,data.address);
+    printf("名前：%s\n",data.name);
+    printf("性別(1:男性　2:女性)：%d\n",data.gender);
+    printf("年齢：%d\n",data.age);
+    printf("生年月日：%d\n",data.birthday);
+    printf("住所：%s\n",data.address);
 }
